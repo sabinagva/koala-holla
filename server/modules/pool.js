9b5b8454faf3136342803923f81e3da6c88558ca
Maxwell Chrysler <maxwellchrysler@hotmail.com>
2023-05-18 15:04:14 -0500

    modules pool created

---
+++ server/modules/pool.js
@@ -0,0 +1,12 @@
+const pg = require('pg');
+
+const pool = new pg.Pool({
+
+    database: 'koala_holla',
+
+    host: 'localhost' ,
+
+    port: 5432
+})
+
+module.exports = pool;
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const koalaRouter = express.Router();
 
+const pool = require('./')
+
 // DB CONNECTION
+
 
 
 // GET
