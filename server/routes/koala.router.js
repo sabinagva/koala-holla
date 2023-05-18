5fd341f3ec54704dc7511ab5dcec05d96f3cd59b
Maxwell Chrysler <maxwellchrysler@hotmail.com>
2023-05-18 15:12:21 -0500

    server route.get

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const koalaRouter = express.Router();
 
-const pool = require('./')
+const pool = require('../modules/pool');
 
 // DB CONNECTION
 
 
 
+
 // GET
+
+koalaRouter.get('/',(req,res)=>{
+    let queryText = 'SELECT * FROM "koala";';
+    pool.query(queryText)
+    .then(result => {
+        //send content to client
+        res.send(result.rows);
+    }).catch(error =>{
+        console.log('we have an error in our get', error);
+        res.sendStatus(500);
+    })
+});
+
+
 
 
 // POST
